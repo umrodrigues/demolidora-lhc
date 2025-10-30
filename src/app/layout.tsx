bdae73bf45c2a513f9468e11b0fe2c1ab30dd30a
umrodrigues <umrodrigues@example.com>
2025-10-30 12:38:48 -0300

    fix: adjust hidratatyon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -51,6 +51,13 @@ export const metadata: Metadata = {
   manifest: "/manifest.json",
 };
 
+export const viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
+};
+
 export default function RootLayout({
   children,
 }: Readonly<{
