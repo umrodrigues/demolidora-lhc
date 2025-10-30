6c70ca3229ceef75907f8ab9f36a9afe7e80005c
umrodrigues <umrodrigues@example.com>
2025-10-30 11:35:20 -0300

    fix: change orders

--- src/data/works.ts
+++ src/data/works.ts
@@ -16,11 +16,11 @@ export const works: Record<string, Work> = {
     category: 'Demolição e Terraplenagem',
     images: [
       '/obras/1 - Demolição Predial/01 - Imagem Início .jpg',
-      '/obras/1 - Demolição Predial/02 - Imagem Fim .jpg',
       '/obras/1 - Demolição Predial/03 Portfólio .jpg',
       '/obras/1 - Demolição Predial/04 Portfólio .jpg',
       '/obras/1 - Demolição Predial/05 Portfólio .jpg',
-      '/obras/1 - Demolição Predial/06 Portfólio .jpg'
+      '/obras/1 - Demolição Predial/06 Portfólio .jpg',
+      '/obras/1 - Demolição Predial/02 - Imagem Fim .jpg',
     ]
   },
   '2-unimed': {
@@ -30,12 +30,12 @@ export const works: Record<string, Work> = {
     category: 'Escavação',
     images: [
       '/obras/2 - UNIMED/01 Foto de Inicio .jpg',
-      '/obras/2 - UNIMED/02 Foto de Fim .jpg',
       '/obras/2 - UNIMED/03 Foto .jpg',
       '/obras/2 - UNIMED/04 Foto .jpg',
       '/obras/2 - UNIMED/05 Foto .jpg',
       '/obras/2 - UNIMED/06 Foto .jpg',
-      '/obras/2 - UNIMED/Imagem do WhatsApp de 2025-10-27 à(s) 11.46.36_43c8c3db.jpg'
+      '/obras/2 - UNIMED/Imagem do WhatsApp de 2025-10-27 à(s) 11.46.36_43c8c3db.jpg',
+      '/obras/2 - UNIMED/02 Foto de Fim .jpg',
     ]
   },
   '3-ciee': {
@@ -45,10 +45,10 @@ export const works: Record<string, Work> = {
     category: 'Demolição',
     images: [
       '/obras/3 - Demolição Cieee/01 Foto de Inicio .jpg',
-      '/obras/3 - Demolição Cieee/02 Foto de Fim .jpg',
       '/obras/3 - Demolição Cieee/03 foto .jpg',
       '/obras/3 - Demolição Cieee/04 foto .jpg',
-      '/obras/3 - Demolição Cieee/05 foto .jpg'
+      '/obras/3 - Demolição Cieee/05 foto .jpg',
+      '/obras/3 - Demolição Cieee/02 Foto de Fim .jpg',
     ]
   },
   '4-marcelo-gama': {
@@ -58,11 +58,11 @@ export const works: Record<string, Work> = {
     category: 'Terraplenagem',
     images: [
       '/obras/4 - Escavação Marcelo Gama/01 Foto de Inicio .jpg',
-      '/obras/4 - Escavação Marcelo Gama/02 Foto de Fim .jpg',
       '/obras/4 - Escavação Marcelo Gama/03 foto .jpg',
       '/obras/4 - Escavação Marcelo Gama/04 foto .jpg',
       '/obras/4 - Escavação Marcelo Gama/05 foto .jpg',
-      '/obras/4 - Escavação Marcelo Gama/06 foto .jpg'
+      '/obras/4 - Escavação Marcelo Gama/06 foto .jpg',
+      '/obras/4 - Escavação Marcelo Gama/02 Foto de Fim .jpg',
     ]
   },
   '5-arambare': {
@@ -72,11 +72,11 @@ export const works: Record<string, Work> = {
     category: 'Escavação',
     images: [
       '/obras/5 - Demolição caixa d’água Arambare/01 Foto de Inicio .jpg',
-      '/obras/5 - Demolição caixa d’água Arambare/02 Foto de Fim .jpg',
       '/obras/5 - Demolição caixa d’água Arambare/03 Foto .jpg',
       '/obras/5 - Demolição caixa d’água Arambare/04 Foto .jpg',
       '/obras/5 - Demolição caixa d’água Arambare/05 Foto .jpg',
-      '/obras/5 - Demolição caixa d’água Arambare/06 Foto .jpg'
+      '/obras/5 - Demolição caixa d’água Arambare/06 Foto .jpg',
+      '/obras/5 - Demolição caixa d’água Arambare/02 Foto de Fim .jpg',
     ]
   },
   '6-machopo': {
@@ -86,11 +86,11 @@ export const works: Record<string, Work> = {
     category: 'Demolição',
     images: [
       '/obras/6 - Demolição Chaminés Mapocho/01 Foto de Inicio .jpg',
-      '/obras/6 - Demolição Chaminés Mapocho/02 Foto de Fim .jpg',
       '/obras/6 - Demolição Chaminés Mapocho/03 Foto .jpg',
       '/obras/6 - Demolição Chaminés Mapocho/04 Foto_.jpg',
       '/obras/6 - Demolição Chaminés Mapocho/05 Foto .jpg',
-      '/obras/6 - Demolição Chaminés Mapocho/06 Foto .jpg'
+      '/obras/6 - Demolição Chaminés Mapocho/06 Foto .jpg',
+      '/obras/6 - Demolição Chaminés Mapocho/02 Foto de Fim .jpg',
     ]
   },
   '7-estrutura': {
@@ -100,11 +100,11 @@ export const works: Record<string, Work> = {
     category: 'Demolição',
     images: [
       '/obras/7 - Desmontagem de Estrutura Metálica/01 Foto de Inicio .jpg',
-      '/obras/7 - Desmontagem de Estrutura Metálica/02 Foto de Fim .jpg',
       '/obras/7 - Desmontagem de Estrutura Metálica/03 Foto .jpg',
       '/obras/7 - Desmontagem de Estrutura Metálica/04 Foto .jpg',
       '/obras/7 - Desmontagem de Estrutura Metálica/05 Foto .jpg',
-      '/obras/7 - Desmontagem de Estrutura Metálica/06 Foto .jpg'
+      '/obras/7 - Desmontagem de Estrutura Metálica/06 Foto .jpg',
+      '/obras/7 - Desmontagem de Estrutura Metálica/02 Foto de Fim .jpg',
     ]
   },
   '8-parker': {
